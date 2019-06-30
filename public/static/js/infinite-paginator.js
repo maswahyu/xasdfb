0b9a0d4f149a2c70b3be3458df3f1eeb99e7f770
hermanet lay <laychinhan@gmail.com>
2019-06-30 14:17:20 +0800

    update drilldown menu for mobile

--- public/static/js/infinite-paginator.js
+++ public/static/js/infinite-paginator.js
@@ -55,7 +55,7 @@ Hector_infinitePaginator.prototype = {
             });
             self.currentPage++;
 
-            if(parsedData.pagination.total_pages < self.currentPage){
+            if(parsedData.total_page < self.currentPage){
                 self.$trigger.hide();
             }
         }).always(function () {
