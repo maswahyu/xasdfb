7686daa6f18e18b0efe52a01e3919914e7bf7de0
Tobi <tobidsn@gmail.com>
2019-07-15 16:39:59 +0700

    #cms

--- app/Http/Controllers/Admin/SlideController.php
+++ app/Http/Controllers/Admin/SlideController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\SlideRequest;
 use App\Slide;
 
 class SlideController extends Controller
-{   
+{
     private $title;
 
     function __construct()
@@ -17,19 +17,19 @@ class SlideController extends Controller
     }
 
     public function index(Request $request)
-    {   
+    {
         return view('_admin.slide.index')->with('title', $this->title);
     }
 
     public function list(Request $request)
-    {   
+    {
         $keyword = $request->get('only');
         if (!empty($keyword)) {
             $slide = Slide::where('title', 'LIKE', '%'.$keyword.'%')->latest()->paginate(10);
         } else {
             $slide = Slide::latest()->paginate(10);
         }
-        
+
         return view('_admin.slide.list', compact('slide','keyword'));
     }
 
@@ -39,9 +39,9 @@ class SlideController extends Controller
     }
 
     public function store(SlideRequest $request)
-    {   
+    {
         $validated = $request->validated();
-        
+
         $data = Slide::newRecord($request);
 
         return redirect('magic/slide')->with('success', 'Slide added!');
@@ -51,21 +51,21 @@ class SlideController extends Controller
     {
         $slide = Slide::findOrFail($id);
         return view('_admin.slide.edit', compact('slide'))->with('title', $this->title);
-    } 
+    }
 
     public function show($id)
     {
         $slide = Slide::findOrFail($id);
-                
+
         return view('_admin.slide.show', compact('slide'))->with('title', $this->title);
     }
 
     public function update(SlideRequest $request, $id)
     {
         $validated = $request->validated();
-        
+
         $data = Slide::updateRecord($request, $id);
-        
+
         return redirect('magic/slide')->with('success', 'Slide updated!');
     }
 
