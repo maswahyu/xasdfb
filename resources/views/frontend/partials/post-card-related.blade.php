bb8868024c17d40e666b49cdb46f6f7056bd0831
Tobi <tobidsn@gmail.com>
2020-04-15 11:20:15 +0700

    coba tambah isset

--- resources/views/frontend/partials/post-card-related.blade.php
+++ resources/views/frontend/partials/post-card-related.blade.php
@@ -9,7 +9,7 @@
     <div class="post-card__meta post-meta">
 
         <div class="post-meta__category">
-            <a href="{{ $post->category->url }}" alt="{{ $post->category_name }}">
+            <a href="{{ $post->category ? $post->category->url : '/lifestyle/style' }}" alt="{{ $post->category_name }}">
                 <span>{{ $post->category_name }}</span>
             </a>
         </div>
