890f5c71ee1e0d76e5d74d31620dd7e6742ff633
Abhi Adhitya Yustho <abhi.yustho@saga.id>
2020-09-30 18:40:03 +0700

    Change ga to gtm

--- resources/views/frontend/layouts/skeleton.blade.php
+++ resources/views/frontend/layouts/skeleton.blade.php
@@ -37,15 +37,13 @@
         }
     </style>
     @if($siteInfo['analytics_id'])
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id={{ $siteInfo['analytics_id'] }}"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', '{{ $siteInfo['analytics_id'] }}');
-    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','{{ $siteInfo['analytics_id'] }}');</script>
+    <!-- End Google Tag Manager -->
     @endif
     {!! $siteInfo['headercode'] !!}
     <style>
@@ -136,6 +134,12 @@
 
 
 <body class="site-body {{ $bodyClass ?? '' }}">
+    @if($siteInfo['analytics_id'])
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $siteInfo['analytics_id'] }}"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    @endif
     {!! $siteInfo['bodycode'] !!}
     @include('frontend.layouts.after-body')
 
