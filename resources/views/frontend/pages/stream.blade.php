44638b7a12311ede1ee7cf7ad73e9ba1c71b1ba9
Muhamad Nasir <bhio231nasir@gmail.com>
2020-10-15 16:44:25 +0700

    showmore and greeting text

--- resources/views/frontend/pages/stream.blade.php
+++ resources/views/frontend/pages/stream.blade.php
@@ -17,9 +17,14 @@ $contentClass = 'd-none'
         <div class="stream__video__inner">
           {{-- <iframe src="https://www.youtube.com/embed/5qap5aO4i9A?autoplay=1&controls=1&modestbranding=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> --}}
         </div>
-        <div class="stream__video__desc">
+        <div class="stream__video__desc" :class="{ 'more' : showMore }" @click.prevent="showMore = !showMore">
+          <span class="btn-more">
+            <svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2.53803 0L0 2.53803L8 10.538L16 2.53803L13.462 0L8 5.46197L2.53803 0Z" fill="#9B9B9B"/>
+            </svg>
+          </span>
           <div class="stream__video__caption">
-            <h4 class="title__video">Live stream Event @ Lazone.ID</h4>
+            <h4 class="title__video">Live stream Event @ Lazone.ID @ Lazone.ID</h4>
             <span class="subtitle__video">LIVE PADA 2 OKTOBER 2020</span>
           </div>
           <div class="stream__video__subs">
@@ -53,8 +58,8 @@ $contentClass = 'd-none'
           <div class="screen-chat screen-chat--center screen-chat--white">
             <div class="login-bar">
               <p class="mb-3">Kamu belum login untuk memulai chat</p>
-              <a href="{{ url('member/login') }}" class="button button-carrot">Login</a> <br>
-              <span>Atau</span> <br>
+              <a href="{{ url('member/login') }}" class="button button-carrot">Login</a>
+              <span>Atau</span>
               <button
                 class="button button-black"
                 @click="loginGuest"
@@ -70,7 +75,7 @@ $contentClass = 'd-none'
           >
             <div
               v-if="showGuestForm"
-              class="screen-chat screen-chat--white p-20"
+              class="screen-chat screen-chat--white p-20 form__login"
               :class="{'screen-chat--active': showGuestForm }"
             >
               <div class="form-login">
@@ -146,6 +151,18 @@ $contentClass = 'd-none'
                     <strong v-else>@{{ chat.name }} @{{ chat.status == 2 ? '(Tamu)': null }}</strong>
                     @{{ chat.message }}
                   </span>
+                </div>
+              </div>
+              {{-- Greeting --}}
+              <div v-if="greeting" class="greeting">
+                <div class="greeting__icon">
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M23.0354 6.50902C22.4257 5.26172 21.5294 4.20506 20.3715 3.36844C18.3189 1.88545 15.4241 1.10156 12 1.10156C8.57592 1.10156 5.68111 1.88541 3.62855 3.36844C2.47064 4.20506 1.57434 5.26172 0.964594 6.50902C0.324562 7.81833 0 9.34242 0 11.0391C0 14.7086 1.51336 17.5159 4.3882 19.2045C3.99052 20.6075 3.47452 21.4577 3.46988 21.4653L2.58005 22.896L4.26544 22.8984H4.27031C4.27514 22.8984 4.28006 22.8984 4.28527 22.8984C4.81716 22.8984 7.25742 22.7837 9.24159 20.7933C10.1163 20.915 11.0418 20.9766 12 20.9766C15.4241 20.9766 18.3189 20.1927 20.3715 18.7097C21.5294 17.873 22.4257 16.8164 23.0354 15.5691C23.6754 14.2598 24 12.7357 24 11.0391C24 9.34242 23.6754 7.81828 23.0354 6.50902ZM19.2733 17.1899C17.5424 18.4405 15.0273 19.1016 12 19.1016C10.9598 19.1016 9.96727 19.0226 9.04992 18.8668L8.52792 18.7781L8.1825 19.1794C7.44028 20.0414 6.58341 20.5017 5.856 20.7468C6.04491 20.2372 6.2362 19.6256 6.39056 18.9287L6.5497 18.2106L5.89003 17.8852C3.22584 16.5711 1.875 14.2677 1.875 11.0391C1.875 8.32486 2.83444 6.25542 4.72669 4.88827C6.45759 3.63764 8.97267 2.97656 12 2.97656C15.0273 2.97656 17.5424 3.63759 19.2733 4.88827C21.1656 6.25542 22.125 8.32491 22.125 11.0391C22.125 13.7532 21.1656 15.8227 19.2733 17.1899Z" fill="#EC2427"/>
+                    <path d="M15 11.5L9.75 14.5311L9.75 8.46891L15 11.5Z" fill="#EC2427"/>
+                  </svg>
+                </div>
+                <div class="greeting__text">
+                  Selamat Datang Di Live Chat
                 </div>
               </div>
             </div>
@@ -313,6 +330,8 @@ $contentClass = 'd-none'
       showGuestForm: false,
       showChat: false,
       blocked: false,
+      greeting: true,
+      showMore: false,
       colorCache: {},
       chats: [],
     },
@@ -345,10 +364,17 @@ $contentClass = 'd-none'
         this.login = true
       },
       joinChat: function() {
+        let _vm = this
         this.login = false
         this.scrollToBottom()
         this.showChat = true
         this.connection.status = STATUS_CONNECTING;
+        //hide greeting in 3 sec
+        this.$nextTick(() => {
+          setTimeout(() => {
+            _vm.greeting = false
+          }, 3000)
+        })
         socket.emit('chat.join', {
           streamId: streamId,
           user: {
@@ -361,6 +387,7 @@ $contentClass = 'd-none'
           if (response.joined) {
             chatApp.$data.connection.status = STATUS_CONNECTED;
             chatApp.startTimer();
+            _vm.scrollToBottom()
           }
         });
       },
