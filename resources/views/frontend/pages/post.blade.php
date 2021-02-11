f298ecd9f66350021f8b8d130a7d86640022f940
Abhi Yustho <abhiyustho@gmail.com>
2021-02-11 13:10:36 +0700

    Add facebook pixel only for specific news slug 'chandra-liow-siap-memberikan-ilmunya-di-lensa-academy-2021-Rrg9A'

--- resources/views/frontend/pages/post.blade.php
+++ resources/views/frontend/pages/post.blade.php
@@ -10,6 +10,29 @@
 @section('head_image', imageview(str_replace(' ', '%20', $post->image)))
 @section('head_url', $post->url)
 
+{{-- @since 11-02-2021 --}}
+@if($post->slug === 'chandra-liow-siap-memberikan-ilmunya-di-lensa-academy-2021-Rrg9A')
+    @section('inside-head')
+        <!-- Facebook Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2491952821022917');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=2491952821022917&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Facebook Pixel Code -->
+    @endsection
+@endif
+
 @section('content')
 
 {{-- Ads Placement --}}
