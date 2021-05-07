2d4c8250c35d9dfb654e8b4ce48f71c39412246c
Abhi Adhitya Yustho <abhi.yustho@saga.id>
2021-05-07 10:45:40 +0700

    Ganti tahun

--- resources/views/frontend/pages/tukarlangsung.blade.php
+++ resources/views/frontend/pages/tukarlangsung.blade.php
@@ -21,7 +21,7 @@
         <h5 class="title text-center text-black">
           Periode
         </h5>
-        <p class="title__sub text-site text-center">7 s.d 31 Mei 2020</p>
+        <p class="title__sub text-site text-center">7 s.d 31 Mei 2021</p>
         <br>
         <div class="text-center">
           <a
@@ -40,7 +40,7 @@
         <br>
         <ul class="list-syarat">
           <li>Program ini hanya bisa diikuti oleh Warga Negara Indonesia yang berusia minimal 18 tahun yang tinggal di Indonesia dan memiliki KTP yang berlaku.</li>
-          <li>Program berlaku mulai tanggal 7 Mei 2020 s.d 31 Mei 2020.</li>
+          <li>Program berlaku mulai tanggal 7 Mei 2021 s.d 31 Mei 2021.</li>
           <li>Program ini hanya berlaku kepada member yang telah melakukan Complete Profile dan terverfikasi di profil <a href="https://www.lazone.id/" class="text-site">LAZone.id</a></li>
           <li>Untuk mengikuti program ini, member harus melakukan redeem kupon undian di halaman ini.</li>
           <li>Member diperbolehkan untuk me-redeem kupon undian lebih dari satu.</li>
